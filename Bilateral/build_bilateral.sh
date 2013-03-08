54897e1cef6966045b1d04b7b50f32ead0e22440
Matthew Davidson <sunyata@gmail.com>
2013-03-08 01:41:48 -0500

    Updated build script to reflect gcc 4.5 requirement

--- Bilateral/build_bilateral.sh
+++ Bilateral/build_bilateral.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-g++ -g -ggdb -c *.cpp
-g++ *.o -static-libstdc++ -o BilateralBFS
+
+#g++ -g -ggdb -c *.cpp
+#g++ *.o -static-libstdc++ -o BilateralBFS
+
+g++-4.5 -g -ggdb -c *.cpp
+g++-4.5 *.o -static-libstdc++ -o BilateralBFS
